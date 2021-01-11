bcd61742df3cbcacc0fe0683b71a56e7f26d042e
Andrei <themiticadi@gmail.com>
2021-01-12 00:56:00 +0100

    Convert track task to ISR function

--- lib/Axis_RA/axis_RA.c
+++ lib/Axis_RA/axis_RA.c
@@ -6,7 +6,7 @@ void Axis_RA__init(Axis_RA *self, int pinDir, int pinStep, int pinEnable) {
     self->pinEnable = pinEnable;
 
     self->pos = 0;
-    Axis_RA__driver_speed(self, 10);
+    Axis_RA__driver_speed(self, 1000);
 
     gpio_pad_select_gpio(self->pinDir);
     gpio_pad_select_gpio(self->pinStep);
@@ -37,16 +37,12 @@ void Axis_RA__driver_speed(Axis_RA *self, int speed) {
     self->stepTime = 1000000 / 2 / self->speed;
 }
 
-void IRAM_ATTR task_track_isr_handler(void *arg) {
-    xTaskNotifyFromISR(axis_RA_track_handle, 1, eSetValueWithOverwrite, NULL);
-}
-
 void Axis_RA__task_track_handle(Axis_RA *self, TaskHandle_t task_track_handle, track_op operation) {
     if (task_track_handle == NULL) {
         ESP_LOGE("task_track_RA", "Handler not found");
     }
 
-    printf("[%d] Pos: %d\n", xTaskGetTickCount(), self->pos);
+    printf("[%d] Pos: %d\n", xTaskGetTickCount() * 10, self->pos);
 
     switch (operation) {
     case Start:
@@ -55,19 +51,19 @@ void Axis_RA__task_track_handle(Axis_RA *self, TaskHandle_t task_track_handle, t
     case Resume:
         printf("[%d] RESUME TRACKING\n", xTaskGetTickCount());
         Axis_RA__driver_enable(self, true);
-        ESP_ERROR_CHECK(esp_timer_start_periodic(task_track_timer, self->stepTime));
+        ESP_ERROR_CHECK(esp_timer_start_periodic(track_timer, self->stepTime));
         vTaskResume(task_track_handle);
         break;
     case Stop:
         printf("[%d] STOP TRACKING\n", xTaskGetTickCount());
-        ESP_ERROR_CHECK(esp_timer_stop(task_track_timer));
+        ESP_ERROR_CHECK(esp_timer_stop(track_timer));
         Axis_RA__driver_enable(self, false);
         vTaskSuspend(task_track_handle);
         break;
     case Delete:
         printf("[%d] REMOVE TRACKING\n", xTaskGetTickCount());
-        ESP_ERROR_CHECK(esp_timer_stop(task_track_timer));
-        ESP_ERROR_CHECK(esp_timer_delete(task_track_timer));
+        ESP_ERROR_CHECK(esp_timer_stop(track_timer));
+        ESP_ERROR_CHECK(esp_timer_delete(track_timer));
         Axis_RA__driver_enable(self, false);
         vTaskDelete(task_track_handle);
         break;
@@ -83,21 +79,16 @@ void Axis_RA__task_track_handle(Axis_RA *self, TaskHandle_t task_track_handle, t
 void Axis_RA__task_track(Axis_RA *self) {
     printf("[%d] START TRACKING\n", xTaskGetTickCount());
 
-    // set timer
-    const esp_timer_create_args_t task_track_timer_args = {
-        .callback = &task_track_isr_handler,
-        .name     = "task_track_timer" // name is optional, but may help identify the timer when debugging
-    };
-    ESP_ERROR_CHECK(esp_timer_create(&task_track_timer_args, &task_track_timer));
-    ESP_ERROR_CHECK(esp_timer_start_periodic(task_track_timer, self->stepTime));
-    Axis_RA__driver_enable(self, true);
+    // while (xTaskNotifyWait(0, ULONG_MAX, NULL, portMAX_DELAY)) {
+    // }
+}
 
-    while (xTaskNotifyWait(0, ULONG_MAX, NULL, portMAX_DELAY)) {
-        self->step = !self->step;
-        gpio_set_level(self->pinStep, self->step);
+// Constant steps at specified speed
+void IRAM_ATTR track_isr(Axis_RA *self) {
+    self->step = !self->step;
+    gpio_set_level(self->pinStep, self->step);
 
-        if (!self->step) {
-            self->dir ? (self->pos++) : (self->pos--);
-        }
+    if (!self->step) {
+        self->dir ? (self->pos++) : (self->pos--);
     }
 }
--- lib/Axis_RA/axis_RA.h
+++ lib/Axis_RA/axis_RA.h
@@ -7,8 +7,7 @@
 #include "freertos/semphr.h"
 #include "freertos/task.h"
 
-TaskHandle_t       axis_RA_track_handle;
-esp_timer_handle_t task_track_timer;
+esp_timer_handle_t track_timer;
 
 typedef enum { Start,
                Resume,
@@ -28,6 +27,8 @@ typedef struct {
 void Axis_RA__init(Axis_RA *self, int pinDir, int pinStep, int pinEnable);
 void Axis_RA__task_track(Axis_RA *self);
 void Axis_RA__task_track_handle(Axis_RA *self, TaskHandle_t handle, track_op operation);
+
+void IRAM_ATTR track_isr(Axis_RA *self);
 
 void Axis_RA__driver_enable(Axis_RA *self, bool toEnable);
 void Axis_RA__driver_direction(Axis_RA *self, bool dir);
